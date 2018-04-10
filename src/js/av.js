aed098ce8e94d3a3d38c0d4e71d64bd4c8b184c2
meiya <meiya90hou@163.com>
2018-04-10 23:07:58 +0800

    项目初始化

---
+++ src/js/av.js
@@ -0,0 +1,15 @@
+var APP_ID = 'y9iSz4ngn5DBQckj4n7XPuK3-gzGzoHsz';
+var APP_KEY = 'Ltk9Nr5hXYQVINQ5Pu2noYeU';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+
+// var TestObject = AV.Object.extend('TestObject');
+// var testObject = new TestObject();
+// testObject.save({
+//     words: 'Hello World!'
+// }).then(function(object) {
+//     alert('LeanCloud Rocks!');
+// })
