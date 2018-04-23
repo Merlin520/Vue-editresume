a638903a2ff1d881fb7fd4a7f022cedce5412fa3
meiya <meiya90hou@163.com>
2018-04-23 20:01:58 +0800

    组件

--- src/js/av.js
+++ src/js/av.js
@@ -6,3 +6,46 @@ AV.init({
     appKey: APP_KEY
 });
 
+
+Vue.component('editable-span', {
+    props:['value'],
+    template:
+        `
+        <span>
+            <span v-show="!editing">{{value}}</span>
+            <input v-show="editing" type="text" >
+            <button v-on:click="editing = !editing">edit</button>
+        </span>
+        `,
+
+    data(){
+        return{
+            editing:false,
+        }
+    }
+});
+
+
+
+var app = new Vue({
+
+    el: '#app',
+    data: {
+        editingName:false,
+      resume:{
+          name:'姓名',
+          gender:'男',
+          birthday:'19930121',
+          jobTitle:'前端开发',
+          phone:'12312312541234',
+          email:'23124@12313.com'
+      }
+    },
+
+    methods: {
+        x(e){
+          e.preventDefault();
+          this.resume.name = e.target.innerText;
+        }
+    },
+  });
